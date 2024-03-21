f8d1d19ecf6993cd2742bbe0d5fb0135d06eb8b5
healthpotionstudios <distractedcoder.yt@gmail.com>
2024-03-21 12:20:14 -0500

    Initial version 1.1 (0309)

---
+++ Jacksbox-Python-Data/server-TriviaRacetrack.py
@@ -0,0 +1,240 @@
+from flask import Flask, render_template
+from flask_sockets import Sockets
+import socket as socketLibrary#ip address
+import os, sys
+import csv
+import threading
+import random
+import subprocess
+import json
+
+#################################################################################################
+#################################################################################################
+####                                                                                         ####
+#### Update ADDRESS to your IP address if the auto IP detection does not work. Wifi Specific ####
+#### Settings found in ..\Jackbox-Game-Data\Jackbox Game_Data\StreamingAssets\Settings.json  ####
+####                                                                                         ####
+#################################################################################################
+#################################################################################################
+
+
+
+
+SERVER_TYPE="TriviaRacetrack"
+
+app = Flask(__name__)
+sockets = Sockets(app)
+
+stop_threads = False
+allTrivia = []
+currentlyUsedTrivia = [] #this will be up to 500 previous questions, insert only first
+currentlyGeneratingTTS = False;
+loserCallouts = []
+
+
+#load IP address from unity settings
+with open('../Jackbox-Game-Data/Jackbox Game_Data/StreamingAssets/Settings.json', 'r', encoding='utf-8') as file:
+    settingsJsonData = json.load(file)
+
+address = settingsJsonData["ServerSettings"]["Data"]["Address"]
+port = settingsJsonData["ServerSettings"]["Data"]["Port"]
+maskIpAddress = settingsJsonData["ServerSettings"]["Data"]["DontDisplayIpAddress"]
+
+
+# List to keep track of connected WebSocket clients
+connected_sockets = []
+
+@sockets.route('/websocket')
+def websocket(ws):
+    global allTrivia, currentlyUsedTrivia, currentlyGeneratingTTS,connected_sockets
+    connected_sockets.append(ws)
+    try:
+        while not ws.closed:
+            original_message = ws.receive()
+            if original_message:
+                print('Received message:', original_message)
+                message = original_message.split(";")
+                
+                if (message[0] == "unity_validate"):
+                    #reset connected sockets
+#                    connected_sockets = []
+#                    connected_sockets.append(ws)
+                    #send server name back to unity
+                    for socket in connected_sockets:
+                        socket.send("python_server;" + SERVER_TYPE)
+                    
+                if (message[0] == "unity_01_generate_questions"):
+                    #get total questions and begin generation
+                    #threaded
+                    total = int(message[1])
+                    generateTTSThread = threading.Thread(target=lambda: PickTriviaAndGenerateTTS(total), daemon=True)
+                    generateTTSThread.start()
+                
+                if (message[0] == "unity_01_stop_generate_questions"):
+                    #stop TTS generation if necessary
+                    currentlyGeneratingTTS = False
+                    connected_sockets = []
+                    
+                
+                if (message[0] == "unity_02_get_question"):
+                    #send question
+                    #       0                1        2         3         5       6
+                    #["Which of ... fur?","Reptile","Bird","Amphibian","Mammal*",""]
+                    current = int(message[1])
+                    for socket in connected_sockets:
+                        socket.send("python_03_send_question;" + ";".join(allTrivia[currentlyUsedTrivia[current]]))
+#                        socket.send("python_03_send_question;" + ";".join(allTrivia[2])) #temp
+                    
+                if (message[0] == "unity_confirmation_login"):
+                    #threaded
+                    threading.Thread(target=lambda: GenerateLoserPlayerCallout(message[1]), daemon=True).start()
+                
+                #message is forwarded
+                if (message[0] == "unity_reload_webpage"): #only for new games
+                    #forward message
+                    for socket in connected_sockets:
+                        socket.send(original_message)
+                    #reset connected sockets
+                    connected_sockets = []
+                        
+                if (message[0] == "phone_00_login"):
+                    #forward message
+                    for socket in connected_sockets:
+                        socket.send(original_message)
+                        
+                if (message[0] == "unity_error" or message[0] == "unity_confirmation_login"):
+                    #forward message
+                    for socket in connected_sockets:
+                        socket.send(original_message)
+                
+                if (message[0] == "phone_skip_intro"):
+                    #forward message
+                    for socket in connected_sockets:
+                        socket.send(original_message)
+                
+                if (message[0] == "unity_04_send_answers"):
+                    #forward message
+                    for socket in connected_sockets:
+                        socket.send(original_message)
+                        
+                if (message[0] == "phone_05_send_answer"):
+                    #forward message
+                    for socket in connected_sockets:
+                        socket.send(original_message)
+                    
+                if (message[0] == "unity_reset_to_waiting"):
+                    #forward message
+                    for socket in connected_sockets:
+                        socket.send(original_message)
+                
+
+    except Exception as e:
+        print(e)
+    finally:
+        # Remove the WebSocket from the list when the connection is closed
+        connected_sockets.remove(ws)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+def LoadTriviaQuestions():
+    global allTrivia
+    data = ""
+    with open("game_data/TriviaRacetrack/trivia_all.txt", 'r', encoding='utf-8') as content_file:
+        data = content_file.read().split("\n")
+    
+    for i in range(len(data)):
+        data[i] = data[i].strip().split(';')
+        #all trivia is correct length
+#        if (len(data[i]) != 6):
+#            print(i,data[i])
+    
+    allTrivia = data
+    
+    #threaded, aborted only if unity game starts the generation, backs out of the game, and loads back in
+def PickTriviaAndGenerateTTS(total):
+    global allTrivia, currentlyUsedTrivia, currentlyGeneratingTTS
+    currentlyGeneratingTTS = True
+    print("past trivia:",len(currentlyUsedTrivia))
+    for i in range(total):
+        temp = random.randint(0,len(allTrivia))
+        while (temp in currentlyUsedTrivia): #avoid duplicates
+            temp = random.randint(0,len(allTrivia))
+        currentlyUsedTrivia.insert(0,temp)
+        
+    currentlyUsedTrivia = currentlyUsedTrivia[:500]
+    
+    for i in range(total):
+        command = "echo \"" + CleanTextForPiper(allTrivia[currentlyUsedTrivia[i]][0]) + "\" | .\\piper\\piper.exe -m .\\piper\\voices\\en_US-ryan-medium.onnx -f piper\\game_data\\TriviaRacetrack\\trivia-" + str(i) + ".wav --noise_w 1.2 -q"
+
+        # Use subprocess.run() without check=True to wait for the command to complete
+        if (currentlyGeneratingTTS):
+            #this is how we abort the thread
+            subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+            if (i == 0):
+                print("question 1 generated")        
+    if (currentlyGeneratingTTS):
+        print("all audio generated")
+    currentlyGeneratingTTS = False
+    
+    #threaded
+def GenerateLoserPlayerCallout(name):
+    message = random.choice(loserCallouts)
+    command = "echo \"" + CleanTextForPiper(message.replace("{0}",name)) + "\" | .\\piper\\piper.exe -m .\\piper\\voices\\en_US-ryan-medium.onnx -f piper\\game_data\\TriviaRacetrack\\trivia-callout-" + name.replace(":","") + ".wav --noise_w 1.0 -q"
+    subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        
+    
+def CleanTextForPiper(string):
+    #accents don't appear in powershell. didn't feel like figuring out if it worked from python
+    string = string.replace("é","e").replace("ó","o").replace("ô","o").replace("ø","o").replace("ú","u").replace("û","u").replace("ć","c")
+    #quotes kill the echo command
+    string = string.replace("\'","").replace("\"","").replace("‘","").replace("’","").replace("“","").replace("”","").replace("\r"," ").replace("\n"," ").replace(":"," ")
+    return string.strip()
+
+
+if __name__ == '__main__':
+    if not os.path.exists("piper/game_data/TriviaRacetrack/"):
+        os.makedirs("piper/game_data/TriviaRacetrack/")
+    
+    
+    # auto collect IP of machine and update secret.js
+    if (address.lower() == "x.x.x.x"):
+        ip_address = socketLibrary.gethostbyname(socketLibrary.gethostname())
+        address = ip_address
+    
+    #update secret data from /templates/js/
+    secretfile=open("static/js/secret.js","w")
+    secretfile.write('address = "' + address + '";port = "' + str(port) + '";')
+    secretfile.close()
+    
+    
+    #read in all Trivia
+    LoadTriviaQuestions()
+    
+    #load loser callouts
+    with open('game_data/TriviaRacetrack/narrator messages.csv', 'r', encoding='utf-8') as csvfile:
+        csv_reader = csv.reader(csvfile)
+        for row in csv_reader:
+            # 'row' is a list containing the values in each row
+            if ("losercallout-" in row[0]):
+                loserCallouts.append(row[1])
+
+
+
+    try:
+        print("Server for [" + SERVER_TYPE + "]")
+        if (maskIpAddress):
+            print("Server Started at X.X.X.X:" + str(port))
+        else:
+            print("Server Started at " + address + ":" + str(port))
+        from gevent import pywsgi
+        from geventwebsocket.handler import WebSocketHandler
+    
+        server = pywsgi.WSGIServer((address, port), app, handler_class=WebSocketHandler)
+        server.serve_forever()
+    except KeyboardInterrupt:
+        stop_threads = True
+#        color_thread.join()
+        
--- README.md
+++ README.md
@@ -1,1 +1,69 @@
-# Jacks-Box-Project
+# Initial Setup (New Computer)
+
+A setup guide can be found at the end of **this video.** It’s not as difficult as it looks.
+
+1. Download **Anaconda** Python Distribution
+    1. <https://www.anaconda.com/download>
+    2. Install Anaconda with default settings
+2. Open the Start Menu and search for **Anaconda Prompt**
+3. In the terminal window, copy and paste the following command
+    1. `pip install Flask==1.1.4 Flask-Sockets flask-socketio gevent greenlet markupsafe==2.0.1`
+4. Download the Jack’s Box Project
+    1. At the top of this page, there should be a green button that says **Code**▾
+    2. Click that, then select **Download ZIP**
+5. Extract the contents of the ZIP file and move the folder to your Desktop
+    1. _You can put the folder wherever you want. Desktop is easiest_
+6. Download the latest release of **Piper** (free text-to-speech engine)
+    1. <https://github.com/rhasspy/piper/releases>
+    2. Click the download option for **piper_windows_amd64.zip**
+7. Extract the contents of the ZIP file to `{Project}\\Jacks-Box-Project-main\\Jacksbox-Python-Data`
+    1. Folder should now include these folders: game_data, piper, static, templates
+8. Open the new **piper folder** and create a new folder called: **voices**
+    1. Folder should now include these folders: espeak-ng-data, pkgconfig, voices
+9. Download Ryan’s voice
+    1. <https://huggingface.co/rhasspy/piper-voices/tree/v1.0.0/en/en_US/ryan/medium>
+    2. Download both **en_US-ryan-medium.onnx** and **en_US-ryan-medium.onnx.json**
+        1. Make sure the .json file is renamed to **en_US-ryan-medium.onnx.json**
+    3. Move both files into the **piper/voices** folder created earlier
+
+# Changing Settings
+
+If you happen to be streaming this game, **it is very important to hide your IP Address**.
+
+1. Navigate to `{Project}\\Jacks-Box-Project-main\\Jackbox-Game-Data\\Jackbox Game_Data\\StreamingAssets`
+2. Open **Settings.json** (these settings are universal for server and game data)
+    1. **Server Settings:**
+        1. If the game fails to automatically identify your IP address, tweak these lines:
+            1. `"Address": "x.x.x.x",`
+                1. _Type: string_
+            2. `"Port": 80,`
+                1. _Type: integer_
+        2. If you need to mask your IP address in game, tweak this line:
+            1. `"DontDisplayIpAddress": false,`
+                1. _Type: boolean true or false_
+    2. **Trivia Racetrack Settings:**
+        1. `"TotalQuestions"`: Total trivia questions
+        2. `"QuestionsBeforeLoserBonus"`: How many questions before the last place player is given a boost
+        3. `"ShowScoresEveryXQuestions"`: How frequently to show the score table
+
+# Running the Game
+
+1. Open the Start Menu and search for **Spyder** (Spyder looks very complicated but you don’t need to understand anything about the program)
+    1. From Spyder Editor, open `{Project}\\Jacks-Box-Project-main\\Jacksbox-Python-Data\\server-TriviaRacetrack.py`
+    2. Click the green run script button at the top of Spyder
+        1. If you get a popup, just click Ok or Run
+        2. Bottom left corner of Spyder should confirm that the server started
+    3. Minimize the server but keep it running
+        1. If Sypder ever asks about updating, skip the update and disable automatic update checking
+2. Start the game located at `{Project}\\Jacks-Box-Project-main\\Jackbox-Game-Data\\Jackbox Game.exe`
+3. Play the game! The game will display the link to connect to on the phones (unless hidden).
+
+# Known Issues/Details
+
+1. Everyone must be on the same WiFi network, including the computer hosting the server/game. Keep in mind that if your router broadcasts both a 4G and 5G connection, those are considered separate networks.
+2. This is a local server only. There is no access available from the internet.
+3. If the announcer fails to read a question, there is a 15 second failsafe in place. This is rare but will not result in the game crashing.
+4. Make sure you keep the browser window open the entire time on your phone. Leaving the game in the background may result in missing a question or desyncing.
+5. If you are skipped for a question, sorry but you should get the next question once it is ready.
+6. The game will very rarely crash, if this happens, there is a button in the bottom right to skip to the results screen.
+    **1. If the server crashes, you must close the server and restart. You do not have to close the game, simply back out to the title screen.**
